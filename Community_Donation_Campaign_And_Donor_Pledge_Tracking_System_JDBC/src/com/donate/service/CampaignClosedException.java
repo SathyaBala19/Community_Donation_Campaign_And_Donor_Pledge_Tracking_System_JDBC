0bb7f6d11ab9d24dd2b6fd5b1839d30e19eae45d
SathyaBala19 <sathyabala1934@gmail.com>
2026-06-29 19:46:27 +0530

    commit

--- Community_Donation_Campaign_And_Donor_Pledge_Tracking_System_JDBC/src/com/donate/service/CampaignClosedException.java
+++ Community_Donation_Campaign_And_Donor_Pledge_Tracking_System_JDBC/src/com/donate/service/CampaignClosedException.java
@@ -1,6 +1,11 @@
 package com.donate.service;
 
 public class CampaignClosedException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public CampaignClosedException(String message) {
 		super(message);
 	}
